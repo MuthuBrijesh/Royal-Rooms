8bdda3b5910c31d9cd8a3cd8136d1e000552c997
MuthuBrijesh <20cs030@ritrjpm.ac.in>
2023-05-04 14:43:26 +0530

    hi

--- admin/src/Cust.js
+++ admin/src/Cust.js
@@ -1,17 +1,16 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './Cust.css';
 import background from "./img/14.jpg";
 
 const Cust = () =>{
     const [data,setData]=useState([]);
-    const handleSubmit = (e) =>{
+    useEffect (()=>{
       fetch("http://localhost:5000/custret", { method: "POST", crossDomain: true,
       headers: { "Content-Type": "application/json",
         Accept: "application/json", "Access-Control-Allow-Origin": "*", },
     })
       .then((res) => res.json())
-      .then((data) => {  
-        console.log("Hello")
+      .then((data) => { 
         if(data.status==="OK"){
           setData(data.data);  
         }
@@ -19,18 +18,17 @@ const Cust = () =>{
           alert("No Result Found");
         } 
       });
-    };
-    function emailid(e){
-      console.log(e);
-    }
-    handleSubmit;
+    });
+    /*function emailid(e){
+      console.log(e); onClick={emailid(i.email)}
+    }*/
     return(
         <div className='cust' style={{ backgroundImage: `url(${background})`, height:800,backgroundSize: 'cover',marginTop:-100}}>
             <form  className='form6'><br></br>
                 <h1>Select Customer</h1><br></br>
-                  { data.map((i, index)=>{
+                  { data.map((i)=>{
                     return(
-                      <div className='reti'>{i.email}<button onClick={emailid(index)}>View</button></div>
+                      <div className='reti'>{i.email}<button>View</button></div>
                     );
                   })
                   }
